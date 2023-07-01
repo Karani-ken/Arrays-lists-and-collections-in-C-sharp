c5f430330d8e70e38c6b84c63dcbd1fdc32e8d8e
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-02 00:06:47 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,47 @@ namespace Arrays_lists_and_collections
     {
         static void Main(string[] args)
         {
+            //arrays
+            var names = new List<string> {"Kenneth","Jimmy","Grace","Miriam","Maureen"};
+            names.Add("Teddy");
+            //names.Remove("Kenneth");
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name}!");               
+
+            }
+            //index
+            var index = names.IndexOf("Kenneth");
+            if(index == -1)
+            {
+                Console.WriteLine($"when a item is not found IndexOf retuns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"the {names[index]} is found at index {index}");
+            }
+
+            //fibonaci numbers
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            Console.WriteLine("Fibonacci numbers");
+            while( fibonacciNumbers.Count < 20)
+            {
+
+                var previous1 = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous1 + previous2);
+            }
+            foreach (var number in fibonacciNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
