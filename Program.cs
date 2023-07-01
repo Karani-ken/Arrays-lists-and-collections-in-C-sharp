e2792853a0c7dfd5945078ef3e353c6e14fa68f0
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-01 22:33:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_lists_and_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
